3ae0a500ff36783d263426edabe61d6100c2523c
ArtemySavostov <savostov322@yandex.ru>
2026-03-16 16:56:57 +0400

    Update api-service

--- api-service/src/main/java/com/savostov/kafka/model/Task.java
+++ api-service/src/main/java/com/savostov/kafka/model/Task.java
@@ -8,15 +8,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Task {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
     private String status;
+    private Long categoryId;
 }
